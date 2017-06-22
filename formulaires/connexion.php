15401ab4a87bb30003f1285854882e583d1b0bab
Sinostra <spanjaard.david@gmail.com>
2017-06-22 12:51:46 +0200

    Ajout des formulaires

---
+++ formulaires/connexion.php
@@ -0,0 +1,41 @@
+<?php
+
+if($_POST) {
+
+	$request = "SELECT * FROM membre WHERE pseudo = ".$_POST['pseudo'];
+	$result = $pdo->query($request);
+
+	if($result->rowCount() >= 1) {
+
+		$membre = $result->fetch(PDO::FETCH_ASSOC);
+
+		if(password_verify($_POST['mdp'], $membre['mdp']) || $_POST['mdp'] === $membre['mdp']) {
+			//Connexion
+			//$_SESSION['membre']
+		}
+
+		else echo "erreur de mot de passe";
+
+	}
+
+	else echo "erreur de pseudo";
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form method="post">
+		<input type="text" name="pseudo" placeholder="Pseudo">
+		<input type="password" name="mdp" placeholder="Mot de passe">
+		<input type="submit" value="Connexion">
+	</form>
+
+</body>
+</html>
---
+++ formulaires/inscription.php
@@ -0,0 +1,42 @@
+<?php
+
+	if($_POST) {
+
+		$request = "INSERT INTO membre (pseudo, mdp, nom, prenom, telephone, email, civilite, statut, date_enregistrement) VALUES (:pseudo, :mdp, :nom, :prenom, :telephone, :email, :civilite, 0, CURDATE())";
+
+		$prep = $pdo->prepare($request);
+
+		foreach ($_POST as $key => $value) {
+			$prep->bindValue(':'.$key, $value, PDO::PARAM_STR);
+		}
+
+		$prep->execute();
+
+	}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form method="post">
+		<input type="text" name="pseudo" placeholder="Pseudo">
+		<input type="password" name="mdp" placeholder="Mot de passe">
+		<input type="text" name="nom" placeholder="Nom">
+		<input type="text" name="prenom" placeholder="Prénom">
+		<input type="text" name="telephone" placeholder="Téléphone">
+		<input type="mail" name="email" placeholder="Mail">
+		<select name="civilite">
+			<option value="h">Homme</option>
+			<option value="f">Femme</option>
+		</select>
+		<input type="submit" value="Inscription">
+	</form>
+
+</body>
+</html>
