12d7fbdb04fa23bdaf2a5d0e3a8013d630ccbeb5
stefygreg <gregoristephanie@gmail.com>
2017-06-22 12:42:07 +0200

    inc_stef

---
+++ inc/bas.inc.php
@@ -0,0 +1,14 @@
+</div> <!-- section/conteneur -->
+	</section>
+		
+
+	<footer>
+		<div class="conteneur">
+			
+		</div> <!-- footer/conteneur -->
+	</footer>	
+
+
+
+</body>
+</html>
---
+++ inc/haut.inc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ANNONCEO</title>
+	<link rel="stylesheet" type="text/css" href="/annonceo/inc/css/style.css">
+</head>
+<body>
+	
+	<header>
+		<div class="conteneur">
+			<span>
+				<a href="#">Annonceo</a>
+			</span>	
+			
+			<nav>
+
+			</nav>
+		</div> <!-- header/conteneur -->
+	</header>
+	
+	<section>
+		<div class="conteneur">
+			
+
+		
