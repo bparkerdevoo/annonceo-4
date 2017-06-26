6f3fd862d5af3d60499cb2745a9ec5a9fc52f7fb
stefygreg <gregoristephanie@gmail.com>
2017-06-26 12:26:34 +0200

    stefy26_2

--- admin/gestion_notes.php
+++ admin/gestion_notes.php
@@ -3,45 +3,6 @@
 require_once("../inc/init.inc.php");
 require_once("../inc/haut.inc.php");
 
-?>
-
-<ul class="notes-echelle">
-	<li>
-	<label for="note01">1</label>
-	<input type="radio" name="noteA" id="note01" value="1">
-	</li>	
-
-	<li>
-	<label for="note02">2</label>
-	<input type="radio" name="noteB" id="note02" value="2">
-	</li>	
-
-	<li>
-	<label for="note03">3</label>
-	<input type="radio" name="noteC" id="note03" value="3">
-	</li>	
-
-	<li>
-	<label for="note04">4</label>
-	<input type="radio" name="noteA" id="note04" value="4">
-	</li>	
-
-	<li>
-	<label for="note05">5</label>
-	<input type="radio" name="noteA" id="note05" value="5">
-	</li>		
-
-</ul>
-
-
-<?php
-require_once("../inc/bas.inc.php");
-
-?>
-
-require_once('../inc/init.inc.php');
-require_once('../inc/haut.inc.php');
-
 if($_POST)
 {
 	foreach ($_POST as $lignes => $valeurs)
@@ -50,8 +11,6 @@ if($_POST)
 		
 	}
 }
-
-
 
 
 ?>
